b1426cb8c8e64bbde0ce3a49c54197d7ec59a609
Satriya <feixiaojendral@gmail.com>
2026-05-22 10:00:49 +0700

    Support Cloudflare Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/e-portofolio-ppg/',
+    base: process.env.CF_PAGES ? '/' : '/e-portofolio-ppg/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
